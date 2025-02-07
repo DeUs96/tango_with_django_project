f88bb99d6e6330ea951d5e03d01297989389678c
yiyang xiao <2901430X@student.gla.ac.uk>
2025-02-07 11:56:25 +0000

    cheaper 7

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rango.models import Page
 from rango.models import Category
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 
 from django.http import HttpResponse
@@ -48,3 +50,18 @@ def show_category(request, category_name_slug):
   return render(request, 'rango/category.html', context=context_dict)
 
 
+def add_category(request):
+  form = CategoryForm()
+  # A HTTP POST?
+  if request.method == 'POST':
+    form = CategoryForm(request.POST)
+  # Have we been provided with a valid form?
+    if form.is_valid():
+    # Save the new category to the database.
+      form.save(commit=True)
+    # Now that the category is saved, we could confirm this.
+    # For now, just redirect the user back to the index view.
+      return redirect('/rango/')
+    else:
+      print(form.errors)
+  return render(request, 'rango/add_category.html', {'form': form})
