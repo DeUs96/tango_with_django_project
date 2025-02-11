4e51fb07abce0dafa3ac9a493b19173316ac1ada
yiyang xiao <2901430X@student.gla.ac.uk>
2025-02-11 14:50:42 +0000

    cheaper 10

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 
@@ -28,7 +29,15 @@ def index(request):
         'categories': category_list,
           'pages': page_list}
   # Render the response and send it back!
-  return render(request, 'rango/index.html', context=context_dict)
+  request.session.set_test_cookie()
+  if request.session.test_cookie_worked():
+    print("TEST COOKIE WORKED!")
+    request.session.delete_test_cookie()
+  response = render(request, 'rango/index.html', context=context_dict)
+  visitor_cookie_handler(request)
+  context_dict['visits'] = request.session['visits']
+
+  return response
 
 def show_category(request, category_name_slug):
 # Create a context dictionary which we can pass
@@ -146,6 +155,30 @@ def user_logout(request):
   logout(request)
   # Take the user back to the homepage.
   return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+  val = request.session.get(cookie)
+  if not val:
+    val = default_val
+  return val
+
+def visitor_cookie_handler(request):
+  visits = int(get_server_side_cookie(request, 'visits', '1'))
+  last_visit_cookie = get_server_side_cookie(request,
+  'last_visit',
+  str(datetime.now()))
+  last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+  '%Y-%m-%d %H:%M:%S')
+  # If it's been more than a day since the last visit...
+  if (datetime.now() - last_visit_time).days > 0:
+    visits = visits + 1
+    # Update the last visit cookie now that we have updated the count
+    request.session['last_visit'] = str(datetime.now())
+  else:
+  # Set the last visit cookie
+    request.session['last_visit'] = last_visit_cookie
+  # Update/set the visits cookie
+  request.session['visits'] = visits
 
 
 
